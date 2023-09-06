5d9bb65e7c647262b1db9dc340fd9e4ef35eaf61
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-09-06 11:28:03 +0400

    Creating home page template and view

--- abstract/views.py
+++ abstract/views.py
@@ -1,3 +1,18 @@
+
+#! Django function and methods
 from django.shortcuts import render
 
 # Create your views here.
+
+
+#!Models,Forms and Serializer classes
+from apps.book.models import Book,BookTitle
+
+
+def getHomeView(request):
+    titlebooks = BookTitle.objects.get(id=2)
+    books = titlebooks.books
+    context = {
+        'books': books
+    }
+    return render(request,'abstract/main.html',context)
