41ab7907e6a640e79cc422c68bf3dffa27d657c6
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-09-07 21:52:16 +0400

    Configuration main page and views

---
+++ abstract/static/abstract/main.js
@@ -0,0 +1,12 @@
+var main = new Vue({
+    el: "#main",
+    delimiters: ['[[', ']]'],
+    data: {
+        main_text:'Hello Main'
+    },
+    methods: {
+        callHello(){
+            console.log('main.jssssssssssss')
+        }
+    },
+})
--- abstract/views.py
+++ abstract/views.py
@@ -1,6 +1,7 @@
 
 #! Django function and methods
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -9,10 +10,33 @@ from django.shortcuts import render
 from apps.book.models import Book,BookTitle
 
 
+
+#?changeTheme
+@login_required
+def changeTheme(request):
+    if 'is_dark_mode' in request.session:                                                                                       
+        request.session['is_dark_mode'] = not request.session['is_dark_mode']
+    else:
+        request.session['is_dark_mode'] = True
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+#!?getHomeView
 def getHomeView(request):
-    titlebooks = BookTitle.objects.get(id=2)
+    #set dark mode
+    # if 'is_dark_mode' in request.session:                                                                                       
+    #     request.session['is_dark_mode'] = not request.session['is_dark_mode']
+    # else:
+    #     request.session['is_dark_mode'] = True
+    
+    #get all books
+    titlebooks = BookTitle.objects.get(id=25)
     books = titlebooks.books
     context = {
         'books': books
     }
     return render(request,'abstract/main.html',context)
+
+
+
+
