4ddfc602243e3a114dda0c2e95a3a320c5d462bf
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-09-08 09:37:50 +0400

    Updating main and view

--- abstract/views.py
+++ abstract/views.py
@@ -2,6 +2,9 @@
 #! Django function and methods
 from django.shortcuts import render,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -12,30 +15,34 @@ from apps.book.models import Book,BookTitle
 
 
 #?changeTheme
-@login_required
+@csrf_exempt
 def changeTheme(request):
     if 'is_dark_mode' in request.session:                                                                                       
         request.session['is_dark_mode'] = not request.session['is_dark_mode']
     else:
         request.session['is_dark_mode'] = True
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return JsonResponse({'is_dark_mode':request.session['is_dark_mode']})
 
 
-#!?getHomeView
-def getHomeView(request):
-    #set dark mode
-    # if 'is_dark_mode' in request.session:                                                                                       
-    #     request.session['is_dark_mode'] = not request.session['is_dark_mode']
-    # else:
-    #     request.session['is_dark_mode'] = True
+
+#?BookTitleListView
+class BookTitleListView(ListView):
+    model = BookTitle
+    template_name = "abstract/main.html"
+    context_object_name = "book_titles"
     
-    #get all books
-    titlebooks = BookTitle.objects.get(id=25)
-    books = titlebooks.books
-    context = {
-        'books': books
-    }
-    return render(request,'abstract/main.html',context)
+    
+    def get(self,request,*args,**kwargs):
+        request.session['page_reload_count'] = 0    
+        page_reload_count = request.session['page_reload_count']
+        
+        if request.method == 'GET':
+            page_reload_count += 1
+            request.session['page_reload_count'] = page_reload_count
+
+        print('Get request coming ', page_reload_count)
+        return super(BookTitleListView,self).get(request, *args, **kwargs)
+    
 
 
 
