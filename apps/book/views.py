ae5e11594392f2141f5e4f8a3b3ebecc0761019e
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-09-11 11:06:58 +0400

    Added form valid function created instance format true or not

--- apps/book/views.py
+++ apps/book/views.py
@@ -1,11 +1,14 @@
 
 #! Django function and methods
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.http import JsonResponse
+from django.views.generic import ListView,FormView
 
 
 #!Models,Forms and Serializer classes
 from .models import Book,BookTitle
+from .forms import BookTitleForm
 
 
 # Create your views here.
@@ -13,12 +16,33 @@ from .models import Book,BookTitle
 
 
 #?BookTitleListView
-class BookListView(ListView):
+class BookListView(FormView):
     model = Book
+    form_class = BookTitleForm
     template_name = "book/book.html"
     context_object_name = "book_titles"
+    # success_url = reverse_lazy('book:books') If you want redirect to other page after some process
+    
+    
+    def get_success_url(self):#If you want modify url before redirect to other page
+        return self.request.path
+    
 
     
     def get_queryset(self):
         return Book.objects.all().order_by('-created')
     
+    
+    
+    @classmethod
+    def is_ajax(cls, request):
+        return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+    
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        if self.is_ajax(self.request) and form.is_valid:
+            instance.save()
+            return JsonResponse({'success': True})
+        return super(BookListView, self).form_valid(form)
+
