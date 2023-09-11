e9e85804a839ea118550e6606434090b2f08e4a3
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-09-11 20:57:02 +0400

    Adding form field validation to book title form,adding bootstarap

--- apps/book/static/book/book.js
+++ apps/book/static/book/book.js
@@ -4,72 +4,80 @@ var book = new Vue({
     data: {
         role:"alert",
         is_complete:false,
+        show:true,
+        is_submit:true
     },
     methods: {
         checkForm(e){
             const self = this
             const book_form = document.getElementById('book_form')
-
             const book_title = document.getElementById('id_book_title') 
             const book_publisher = document.getElementById('id_book_publisher')
             const book_author = document.getElementById('id_book_author')
-
             const csrf = document.getElementsByName('csrfmiddlewaretoken')
 
+            const fd = self.getBookTitleForm(csrf, book_title, book_publisher, book_author)
+            // const checkedForm = self.checkFormFields([csrf[0].value, book_title.value, book_publisher.options[book_publisher.selectedIndex].text, book_author.options[book_author.selectedIndex].text])
+            // const create_book_btn = self.$refs.create_book_btn
+            // console.log('Create book btn is ', create_book_btn);
 
+            if(fd){
+                $.ajax({
+                    type:'POST',
+                    url: book_form.action,
+                    enctype: 'multipart/form-data',
+                    data:fd,
+                    beforeSend : function(){
+                        self.$refs.alert_box.innerHTML = ''
+                        
+                    },
+                    success:function(response){
+                        self.handleAlerts("Book title created succsessfully",'success')
+                    },
+                    error: function(err){
+                        self.handleAlerts("Something went wrong please try again",'danger')
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                })
+            }
+        },
 
-            console.log('Title: ',  book_title)
-            console.log('Publisher Title: ',  book_publisher.options[book_publisher.selectedIndex].text)
-            console.log('Author Title: ', book_author.options[book_author.selectedIndex].text)
-            console.log('Csrf is ', csrf[0]);
-            console.log('Book form ', book_form.action);
-
+        getBookTitleForm(csrf,book_title,book_publisher,book_author){
             const fd = new FormData()
             fd.append('csrfmiddlewaretoken', csrf[0].value)
             fd.append('book_title',book_title.value)
             fd.append('book_publisher',book_publisher.options[book_publisher.selectedIndex].text)
             fd.append('book_publisher',book_publisher.options[book_publisher.selectedIndex].value)
-
             fd.append('book_author',book_author.options[book_author.selectedIndex].text)
             fd.append('book_author',book_author.options[book_author.selectedIndex].value)
-
-
-            
-            $.ajax({
-                type:'POST',
-                url: book_form.action,
-                enctype: 'multipart/form-data',
-                data:fd,
-                beforeSend : function(){
-                    document.getElementById('alert_box').innerHTML = ''
-                },
-                success:function(response){
-                    console.log('Succsess', response)
-                    self.handleAlerts("bg-green-500 text-white font-bold rounded-t px-4 py-2","Succsess","border border-t-0 border-green-400 rounded-b bg-green-100 px-4 py-3 text-green-700","Book title created succsessfully.")
-                },
-                error:function(err){
-                    self.handleAlerts("bg-red-500 text-white font-bold rounded-t px-4 py-2","Error","border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700","Something went wrong please try again.")
-
-                    console.log('Error ', err)
-                },
-                cache: false,
-                contentType: false,
-                processData: false,
-            })
+            return fd
         },
 
-        handleAlerts(notification_title_class,notification_title_text,notification_body_class,notification_body_text){
-            const alert_box = document.getElementById('alert_box')
-            alert_box.innerHTML = `
-                <div role="alert">
-                    <div class="${notification_title_class}">
-                        ${notification_title_text}
-                    </div>
-                    <div class="${notification_body_class}">
-                        <p>${notification_body_text}</p>
-                    </div>
-                </div>
-            `
-        }
+        
+        handleAlerts(text,type){
+            this.$refs.alert_box.innerHTML =  `
+                        <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>${text}</strong> 
+                            <button type="button" class="btn-close fw-bold" data-bs-dismiss="alert" aria-label="Close">X</button>
+                        </div>
+                `
+                this.hideAlertCart()
+        },
+        hideAlertCart(){
+                setTimeout(()=>{
+                    this.$refs.alert_box.innerHTML = ''
+                },3000)
+        },
+
+        // checkFormFields(form_fields_value){
+        //     let count = 0
+        //     form_fields_value.forEach(element => {
+        //         element != '' ? count++ : null 
+        //     });
+        //     this.is_submit=false ? count==form_fields_value.length : null
+        // }
     },
 })
+
