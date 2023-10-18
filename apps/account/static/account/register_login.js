bed94b1f5c3f94c9b08948ef78b03292f37bde6b
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-10-18 09:54:55 +0400

    Finished register design

--- apps/account/static/account/register_login.js
+++ apps/account/static/account/register_login.js
@@ -6,4 +6,9 @@ var register_login = new Vue({
     },
     methods: {
     },
+    mounted() {
+        const sidebar = document.getElementById('sidebar').classList.replace('h-screen','h-full')
+        const phoneInputField = this.$refs.phone
+        const phoneInput = window.intlTelInput(phoneInputField, {utilsScript:"https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",});
+    },
 })
