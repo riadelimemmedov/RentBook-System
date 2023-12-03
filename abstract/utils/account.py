0babf88a52d7cede3dab5e2b9dc36796da82fd8c
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-12-03 22:44:36 +0400

    Creating custom method for handling json response messages for client

--- abstract/utils/account.py
+++ abstract/utils/account.py
@@ -4,10 +4,33 @@
 from django.shortcuts import redirect
 
 
-#?redirect_inactive_users
+# ?redirect_inactive_users
 def redirect_inactive_users(view_func):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated and not request.user.is_active:
             return redirect(request.path)
         return view_func(request, *args, **kwargs)
+
     return wrapper
+
+
+# ?throwMessage
+def throwMessage(message_name):
+    if message_name == "ACTIVE":
+        return {
+            "message": "Signed in successfully",
+            "isLogin": "True",
+            "icon": "success",
+        }
+    if message_name == "INACTIVE":
+        return {
+            "message": "Please activate your account",
+            "isLogin": "False",
+            "icon": "info",
+        }
+    if message_name == "INCORRECT":
+        return {
+            "message": "Email or Password incorrect",
+            "isLogin": "False",
+            "icon": "error",
+        }
